72464713106f9e2d8b30717e77b53512ad1f9e78
nashdevcode <nash.devcode@gmail.com>
2023-04-15 20:42:16 -0500

    Semana2 Terminado

---
+++ Semana02/Semana02/Controllers/AlumnoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Semana02.Models;
+
+
+namespace Semana02.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Calificaciones(Alumno obj)
+        {
+            ViewBag.nota1 = obj.nota1;
+            ViewBag.nota2 = obj.nota2;
+
+            ViewBag.promedio = obj.Promedio();
+
+            ViewBag.condicion = obj.Condicion();
+
+            return View();
+        }
+    }
+}
---
+++ Semana02/Semana02/Models/Alumno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+
+namespace Semana02.Models
+{
+    public class Alumno
+    {
+        public int codigo { get; set; }
+        public String nombre { get; set; }
+        public int nota1 { get; set; }
+        public int nota2 { get; set; }
+
+        public Alumno()
+        {
+
+        }
+
+        public Alumno(int codigo, string nombre, int nota1, int nota2)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+
+        public double Promedio()
+        {
+            return (this.nota1 + this.nota2) / 2.0;
+        }
+
+        public String Condicion()
+        {
+            if (this.Promedio() >= 12.5)
+            {
+                return "aprobado";
+            }
+            else
+            {
+                return "desaprobado";
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
